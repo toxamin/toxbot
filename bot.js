501fea712b9afcea239af9bad2fdac269305f439
toxamin <tuktukmaja@gmail.com>
2017-10-25 17:40:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,12 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on("serverNewMember", function (server, user) {
+    client.addMemberToRole(user, server.roles.get("name", "Member"), function (err) { if (err) console.log(err) })
+
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'whatareyou') {
+    	message.reply('I am a bot that auto-assigns "Member" role to everyone.');
   	}
 });
 
